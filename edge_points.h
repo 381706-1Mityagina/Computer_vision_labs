f1ff971ff690be47566345fc107d1209235f325a
381706-1Mityagina <43111429+381706-1Mityagina@users.noreply.github.com>
2020-11-24 23:20:02 +0300

    Update edge_points.h

--- edge_points.h
+++ edge_points.h
@@ -1,37 +1,37 @@
-﻿#include "methods.h"
+#include "methods.h"
 
-void Moravec(Mat srcImage) {
+void Moravec(Mat src) {
 	int kSize = 5;
 	//int threshold = 140000;
-	int threshold = 380000;  // board
+	int threshold = 400000;  // board
 	int r = kSize / 2;
 	int nCount = 0;
 
-	Mat resMorMat = srcImage.clone(), gray;
-	cvtColor(srcImage, gray, COLOR_BGR2GRAY);
-	Point* pPoint = new Point[srcImage.rows * srcImage.cols];
+	Mat resMorMat = src.clone(), gray;
+	cvtColor(src, gray, COLOR_BGR2GRAY);
+	Point* pPoint = new Point[src.rows * src.cols];
 
-	for (int i = r; i < srcImage.rows - r + 1; i++) {
-		for (int j = r; j < srcImage.cols - r + 1; j++) {
+	for (int i = r; i < src.rows - r + 1; i++) {
+		for (int j = r; j < src.cols - r + 1; j++) {
 			int wV[4] = { 0, 0, 0, 0 };
 			for (int k = -r; k <= r; k++) {
 				for (int m = -r; m <= r; m++) {
-					if (j + m + 1 >= srcImage.cols || i + k >= srcImage.rows || j + m >= srcImage.cols) {
+					if (j + m + 1 >= src.cols || i + k >= src.rows || j + m >= src.cols) {
 						continue;
 					}
 					wV[0] += pow((gray.at<uchar>(i + k, j + m + 1) - gray.at<uchar>(i + k, j + m)), 2);
 
-					if (j + m >= srcImage.cols || i + k + 1 >= srcImage.rows || i + k >= srcImage.rows) {
+					if (j + m >= src.cols || i + k + 1 >= src.rows || i + k >= src.rows) {
 						continue;
 					}
 					wV[1] += pow((gray.at<uchar>(i + k + 1, j + m) - gray.at<uchar>(i + k, j + m)), 2);
 
-					if (i + k + 1 >= srcImage.rows || j + m + 1 >= srcImage.cols || i + k >= srcImage.rows || j + m >= srcImage.cols) {
+					if (i + k + 1 >= src.rows || j + m + 1 >= src.cols || i + k >= src.rows || j + m >= src.cols) {
 						continue;
 					}
 					wV[2] += pow((gray.at<uchar>(i + k + 1, j + m + 1) - gray.at<uchar>(i + k, j + m)), 2);
 
-					if (i + k + 1 >= srcImage.rows || j + m - 1 < 0 || j + m >= srcImage.cols || i + k >= srcImage.rows) {
+					if (i + k + 1 >= src.rows || j + m - 1 < 0 || j + m >= src.cols || i + k >= src.rows) {
 						continue;
 					}
 					wV[3] += pow((gray.at<uchar>(i + k + 1, j + m - 1) - gray.at<uchar>(i + k, j + m)), 2);
@@ -39,7 +39,7 @@ void Moravec(Mat srcImage) {
 			}
 			int value = min(min(wV[0], wV[1]), min(wV[2], wV[3]));
 			if (value > threshold) {
-				if (j < srcImage.rows && i < srcImage.cols) {
+				if (j < src.rows && i < src.cols) {
 					pPoint[nCount] = Point(j, i);
 					nCount++;
 				}
